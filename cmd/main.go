5ba6b8603f7883092942ba4cd8221231d73d7dd3
Natkrita Chumlek <noon.nq@MacBook-Pro.local>
2018-10-15 17:32:33 +0700

    create lineClient and oxfordClient in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	"github.com/sariakra/line-dictionary-bot/pkg/line"
+	"github.com/sariakra/line-dictionary-bot/pkg/bot/line"
+	"github.com/sariakra/line-dictionary-bot/pkg/dictionary/oxford"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.Handle("/api/webhook", line.WebhookHandler())
+	appId := os.Getenv("OXFORD_APP_ID")
+	appKey := os.Getenv("OXFORD_APP_KEY")
+	lineAccessToken := os.Getenv("LINE_ACCESS_TOKEN")
+	lineClient := bot.NewLineClient(bot.LineConfig{
+		BaseUrl: "https://api.line.me/v2",
+		AccessToken: lineAccessToken,
+	})
+	oxfordClient := dictionary.NewOxfordClient(dictionary.OxfordConfig{
+		BaseUrl: "https://od-api.oxforddictionaries.com/api/v1",
+		AppId: appId,
+		AppKey: appKey,
+	}, lineClient)
+	http.Handle("/api/webhook", lineClient.Handler(oxfordClient))
 	http.ListenAndServe(":8090", nil)
 }
